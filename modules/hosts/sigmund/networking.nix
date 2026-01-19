5c53681c94945b68594f69a175bf1ad9460a6233
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-01-18 21:53:05 -0500

    Remove sigmund configuration

--- modules/hosts/sigmund/networking.nix
+++
@@ -1,6 +0,0 @@
-{
-  flake.nixosModules.sigmund = {
-    networking.hostName = "sigmund";
-    networking.networkmanager.enable = true;
-  };
-}
--- modules/hosts/sigmund/users/kerry/sops/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  flake.homeModules."kerry@sigmund" = {config, ...}: {
-    sops = {
-      defaultSopsFile = ./secrets.yaml;
-      defaultSopsFormat = "yaml";
-      age.keyFile = "${config.home.homeDirectory}/.config/sops/age/kerry_sigmund.age";
-    };
-  };
-}
