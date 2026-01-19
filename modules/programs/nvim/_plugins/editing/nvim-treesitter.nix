9526cee44294ab2b4270e7d0b3c313e3f83427e8
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-01-19 14:30:58 -0500

    Fix dependency check for nvim treesitter modules

--- modules/programs/nvim/_plugins/editing/nvim-treesitter.nix
+++ modules/programs/nvim/_plugins/editing/nvim-treesitter.nix
@@ -2,9 +2,18 @@
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       nvim-treesitter.withAllGrammars
-      (nvim-treesitter-context.overrideAttrs (old: {dependencies = [];}))
-      (nvim-treesitter-endwise.overrideAttrs (old: {dependencies = [];}))
-      (nvim-treesitter-textobjects.overrideAttrs (old: {dependencies = [];}))
+      (nvim-treesitter-context.overrideAttrs (old: {
+        dependencies = [];
+        nvimRequireCheck = "";
+      }))
+      (nvim-treesitter-endwise.overrideAttrs (old: {
+        dependencies = [];
+        nvimRequireCheck = "";
+      }))
+      (nvim-treesitter-textobjects.overrideAttrs (old: {
+        dependencies = [];
+        nvimRequireCheck = "";
+      }))
     ];
   };
   xdg.configFile = {
