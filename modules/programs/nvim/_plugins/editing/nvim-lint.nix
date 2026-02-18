ac961cf31159eb94fa23f06809b0c7e42393a6ce
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-02-18 15:56:32 -0500

    fix nvim-lint

--- modules/programs/nvim/_plugins/editing/nvim-lint.nix
+++ modules/programs/nvim/_plugins/editing/nvim-lint.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   programs.neovim.plugins = [pkgs.vimPlugins.nvim-lint];
+  programs.neovim.lazyNixCompat.idOverrides."nvim-lint" = "mfussenegger/nvim-lint";
   xdg.configFile = {
     "nvim/lua/plugins/editing/nvim-lint.lua".source =
       ../../src/lua/plugins/editing/nvim-lint.lua;
