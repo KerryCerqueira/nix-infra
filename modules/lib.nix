879bf1f4fb0fe3b72f56ee8b8da1a9d2af0cf9a9
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-03-09 14:52:30 -0400

    Rename lib, add mkMcp helper

---
+++ modules/lib.nix
@@ -0,0 +1,73 @@
+{
+  flake.lib = let
+    kernelSpecFromEnv = pkgs: pythonEnv:
+      pkgs.writeTextDir
+      "kernels/nix_kernel/kernel.json"
+      (builtins.toJSON {
+        display_name = "Nix Shell Kernel";
+        language = "python";
+        argv = [
+          "${pythonEnv}/bin/python"
+          "-m"
+          "ipykernel_launcher"
+          "-f"
+          "{connection_file}"
+        ];
+        env = {};
+      });
+  in {
+    inherit kernelSpecFromEnv;
+    shellFromEnv = pkgs: pythonEnv: let
+      kernelSpec = kernelSpecFromEnv pkgs pythonEnv;
+    in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pythonEnv
+          jupyter
+        ];
+        shellHook = ''
+          export JUPYTER_PATH="${kernelSpec}:''${JUPYTER_PATH:-}"
+        '';
+      };
+    jupyterAppFromEnv = pkgs: pythonEnv: let
+      kernelSpec = kernelSpecFromEnv pkgs pythonEnv;
+    in
+      pkgs.writeShellApplication {
+        name = "jupyter-data-munging";
+        runtimeInputs = [
+          pkgs.jupyter
+          pythonEnv
+        ];
+        text = ''
+          export JUPYTER_PATH="${kernelSpec}:''${JUPYTER_PATH:-}"
+          exec jupyter lab "$@"
+        '';
+      };
+    mkPythonMcp = {
+      pkgs,
+      uv2nix,
+      pyproject-build-systems,
+    }: {
+      pname,
+      src,
+      entrypoint ? pname,
+    }: let
+      workspace = uv2nix.lib.workspace.loadWorkspace {
+        workspaceRoot = src;
+      };
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+      pythonSet = pkgs.python3.pkgs.overrideScope (
+        pkgs.lib.composeManyExtensions [
+          pyproject-build-systems.overlays.wheel
+          overlay
+        ]
+      );
+      venv = pythonSet.mkVirtualEnv "${pname}-env" workspace.deps.default;
+    in
+      pkgs.writeShellScriptBin pname ''
+        exec ${venv}/bin/${entrypoint} "$@"
+      '';
+  };
+}
