007a5a15866e352c1797d6e942221a6fdaa8f3ca
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-03-09 15:01:56 -0400

    Add new flake inputs for previous commits
    Add input for claude desktop deployment. Add uv2nix and dependencies.
    Remove extra lib import.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     catppuccin.url = "github:catppuccin/nix";
+    claude-desktop = {
+      url = "github:aaddrick/claude-desktop-debian";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     easyeffects-presets = {
       url = "github:JackHack96/EasyEffects-Presets";
       flake = false;
@@ -16,8 +20,23 @@
     nixos-grub-themes.url = "github:jeslie0/nixos-grub-themes";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -29,7 +48,6 @@
           inputs.home-manager.flakeModules.home-manager
           (inputs.import-tree ./modules)
         ];
-        flake.lib = import ./lib.nix;
         systems = ["x86_64-linux"];
       }
     );
