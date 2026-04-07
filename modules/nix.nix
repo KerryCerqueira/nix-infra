a0beabcab8f80b0519f2840e966196163608328c
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-04-07 09:50:59 -0400

    Remove unnecessary overlay

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,19 +5,5 @@
       "flakes"
       "pipe-operators"
     ];
-    nixpkgs.overlays = [
-# Fixes jupytext build failure on unstable 01/19/2024
-      (final: prev: {
-        pythonPackagesExtensions =
-          prev.pythonPackagesExtensions
-          ++ [
-            (python-final: python-prev: {
-              jupytext = python-prev.jupytext.overridePythonAttrs (oldAttrs: {
-                doCheck = false;
-              });
-            })
-          ];
-      })
-    ];
   };
 }
