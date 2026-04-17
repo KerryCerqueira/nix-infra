7db97573952bf85f24dec5f3b29588bf77362b7b
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-04-17 15:47:59 -0400

    Fix constants option

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
             options.flake.lib = lib.mkOption {
               type = lib.types.submodule {
                 freeformType = lib.types.attrsOf lib.types.raw;
-                options.flake.lib.constants = lib.mkOption {
+                options.constants = lib.mkOption {
                   type = lib.types.submodule {
                     freeformType = lib.types.attrsOf lib.types.anything;
                   };
