0223078fcb42c6884d83223e1ce5e0ca05484c30
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-05-11 11:56:16 -0400

    Add opencode

--- modules/opencode.nix
+++ modules/opencode.nix
@@ -1,19 +1,22 @@
-{...}: {
-  flake.homeModules.opencode = {...}: {
-    programs.opencode = {
-      enable = true;
-      settings.provider.ollama = {
-        npm = "@ai-sdk/openai-compatible";
-        name = "Ollama";
-        options = {
-          baseURL = "http://localhost:11434/v1";
-          apiKey = "ollama";
-        };
-        models."qwen3-coder-next:q4_K_M" = {
-          name = "Qwen3 Coder Next";
-          tools = true;
+{self, ...}: {
+  flake.homeModules = {
+    opencode = {...}: {
+      programs.opencode = {
+        enable = true;
+        settings.provider.ollama = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "Ollama";
+          options = {
+            baseURL = "http://localhost:11434/v1";
+            apiKey = "ollama";
+          };
+          models."qwen3-coder-next:q4_K_M" = {
+            name = "Qwen3 Coder Next";
+            tools = true;
+          };
         };
       };
     };
+    "kerry@claudius" = {imports = [self.homeModules.opencode];};
   };
 }
